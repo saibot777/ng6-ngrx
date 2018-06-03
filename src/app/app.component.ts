4bed1f9dc9c87792c856b3cc5cd457996a5b37a8
saibot777 <stefantrajkovic777@gmail.com>
2018-06-04 01:07:55 +0200

    auth selectors implemented...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import {select, Store} from "@ngrx/store";
 import {Observable} from "rxjs";
 import {AppState} from "./reducers";
 import {Logout} from "./auth/auth.actions";
+import {isLoggedIn, isLoggedOut} from "./auth/auth.selectors";
 
 @Component({
   selector: 'app-root',
@@ -10,13 +11,22 @@ import {Logout} from "./auth/auth.actions";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+    isLoggedIn$: Observable<boolean>;
+    isLoggedOut$: Observable<boolean>;
 
-    constructor(private store: Store<AppState>) {
-
-    }
+    constructor(private store: Store<AppState>) {}
 
     ngOnInit() {
 
+      this.isLoggedIn$ = this.store
+        .pipe(
+          select(isLoggedIn)
+        );
+
+      this.isLoggedOut$ = this.store
+        .pipe(
+          select(isLoggedOut)
+        );
 
     }
 
--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -9,7 +9,7 @@ import { environment } from '../../environments/environment';
 
 export interface AppState {}
 
-export const reducers: ActionReducerMap<AppState> = { };
+export const reducers: ActionReducerMap<AppState> = {};
 
 
 export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [] : [];
