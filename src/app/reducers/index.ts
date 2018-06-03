9e63cc105da99cd7efc7ac20d1749929aa8e3b90
saibot777 <stefantrajkovic777@gmail.com>
2018-06-03 23:30:21 +0200

    auth login reducer implemented

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -6,13 +6,35 @@ import {
   MetaReducer
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
+import {User} from "../model/user.model";
+import {AuthActionTypes} from "../auth/auth.actions";
+
+type AuthState = {
+  loggedIn: boolean,
+  user: User
+};
 
 export interface AppState {
+  auth: AuthState;
+}
 
+function authReducer(state: AuthState, action): AuthState {
+  switch (action.type) {
+
+    case AuthActionTypes.LoginAction:
+      return {
+        loggedIn: true,
+        user: action.payload
+      };
+
+    default:
+      return state;
+
+  }
 }
 
 export const reducers: ActionReducerMap<AppState> = {
-
+    auth: authReducer
 };
 
 
