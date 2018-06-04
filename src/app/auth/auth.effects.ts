4bff83c373216a1b3e8962bebc4ee149c1ead4ea
saibot777 <stefantrajkovic777@gmail.com>
2018-06-04 10:03:28 +0200

    effects auth init impl

--- src/app/auth/auth.effects.ts
+++ src/app/auth/auth.effects.ts
@@ -3,6 +3,7 @@ import {Actions, Effect, ofType} from '@ngrx/effects';
 import {AuthActionTypes, Login, Logout} from "./auth.actions";
 import {tap} from "rxjs/internal/operators";
 import {Router} from "@angular/router";
+import {defer, of} from "rxjs";
 
 
 @Injectable()
@@ -14,7 +15,7 @@ export class AuthEffects {
   login$ = this.actions$
     .pipe(
       ofType<Login>(AuthActionTypes.LoginAction),
-      tap(action => localStorage.setItem('user', JSON.stringify(action.user)))
+      tap(action => localStorage.setItem('user', JSON.stringify(action.payload.user)))
     );
 
   @Effect({dispatch: false})
@@ -26,4 +27,16 @@ export class AuthEffects {
         this.router.navigateByUrl('/login');
       })
     );
+
+  @Effect()
+  init$ = defer(() => {
+    const userData = localStorage.getItem('user');
+    let e;
+    if (userData) {
+      return of(new Login(JSON.parse(userData)));
+    } else {
+      return of(new Logout(e));
+    }
+  });
+
 }
