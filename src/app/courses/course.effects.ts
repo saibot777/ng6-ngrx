c129b4a912940833996860b399fa4bd078d96a02
saibot777 <stefantrajkovic777@gmail.com>
2018-06-04 23:01:58 +0200

    effects impl for courses operations

--- src/app/courses/course.effects.ts
+++ src/app/courses/course.effects.ts
@@ -1,7 +1,7 @@
 
 import {Injectable} from "@angular/core";
 import {Actions, Effect, ofType} from "@ngrx/effects";
-import {CourseActionTypes, CourseLoaded, CourseRequested} from "./course.actions";
+import {AllCoursesLoaded, CourseActionTypes, CourseLoaded, CourseRequested} from "./course.actions";
 import {map, mergeMap} from "rxjs/internal/operators";
 import {CoursesService} from "./services/courses.service";
 
@@ -16,5 +16,13 @@ export class CourseEffects {
     mergeMap(action => this.coursesService.findCourseById(action.payload.courseId)),
     map(course => new CourseLoaded({course}))
   );
+
+  @Effect()
+  loadAllCourses$ = this.actions$
+    .pipe(
+      ofType<CourseRequested>(CourseActionTypes.CourseRequested),
+      mergeMap(action => this.coursesService.findAllCourses()),
+      map(courses => new AllCoursesLoaded({courses}))
+    );
 
 }
