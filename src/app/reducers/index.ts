eb926c1216db692129eca52cde2b7eae054b762f
saibot777 <stefantrajkovic777@gmail.com>
2018-06-04 10:14:20 +0200

    implemented storeFreeze meta reducer to ensure state immutability

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -6,10 +6,11 @@ import {
   MetaReducer
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
+import {storeFreeze} from "ngrx-store-freeze";
 
 export interface AppState {}
 
 export const reducers: ActionReducerMap<AppState> = {};
 
 
-export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [] : [];
+export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [storeFreeze] : [];
